42976088e2332a9afe5ad9fc0d35fb94783b29bc
nikos <nikos.efthias@gmail.com>
2017-09-23 05:04:55 +0400

    make map of methodsπ

--- lib/store/filters/filterMethods.go
+++ lib/store/filters/filterMethods.go
@@ -2,20 +2,38 @@ package filters
 
 import (
 	"reflect"
-	"fmt"
+	"regexp"
+	"strconv"
 )
 
-var ValidFilters = []string{
-	"^-$", //closed
-	"^\\d$",
+var Filter = map[*regexp.Regexp]func(*reflect.Value, string){
+	regexp.MustCompile("^-$"):  filterDisableFunc,
+	regexp.MustCompile("^\\d"): setInt,
 }
 
-func FilterDisableFunc(val *reflect.Value) {
-	//reflect.Zero((*val).Type())
-	val.SetInt(13)
-	fmt.Println(val)
+func filterDisableFunc(val *reflect.Value, _ string) {
+	if val.CanSet() {
+		val.Set(reflect.Zero(val.Type()))
+	}
 }
+func setInt(val *reflect.Value, filter string) {
+	s, _ := strconv.Atoi(filter)
 
-func DoFilter(filterSTR string) {
+	switch val.Kind() {
+	case reflect.Ptr:
+		elem:=val.Type().Elem().Kind()
+		_=elem
+		if val.Elem().CanSet() && val.Elem().Kind() == reflect.Int {
+			val.Elem().SetInt(int64(s))
+		} else if val.CanSet() && val.Type().Elem().Kind() == reflect.Int {
+			val.Set(reflect.New(val.Type().Elem()))
+			val.Elem().SetInt(int64(s))
+		}
 
+	case reflect.Int:
+		if val.CanSet() {
+			val.SetInt(int64(s))
+		}
+
+	}
 }
