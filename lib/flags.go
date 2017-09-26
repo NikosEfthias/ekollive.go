d5424577b1219305d30c38d1f8c186375a70092f
nikos <nikos.efthias@gmail.com>
2017-09-26 08:05:27 +0400

    new flags

--- lib/flags.go
+++ lib/flags.go
@@ -1,21 +1,38 @@
 package lib
 
-import "flag"
+import (
+	"flag"
+	"log"
+)
 
 var (
-	FiltersFile *string
-	J           *int
-	Testing     *bool
+	FiltersFile         *string
+	J                   *int
+	Testing             *bool
+	Port                *string
+	DB                  *string
+	Key                 *string
+	BetradarURL         *string
+	BetradarBookmakerId *string
 )
 
 func init() {
-	if !flag.IsParsed() {
+	if !flag.Parsed() {
+		applyFlags()
 		flag.Parse()
+		if *Key == "" || *BetradarBookmakerId == "" {
+			log.Fatalln("No betradar key or bookmakerid is present")
+		}
 	}
 
 }
 func applyFlags() {
+	BetradarBookmakerId = flag.String("BookmakerID", "", "Set betradar bookmaker id")
+	BetradarURL = flag.String("BetradarURL", "liveoddstest.betradar.com:1984", "betradar url to use to get live data")
+	Key = flag.String("KEY", "", "Betradar key to use")
+	DB = flag.String("DB", "root:@tcp(127.0.0.1:3306)/test", "DB address to use")
 	FiltersFile = flag.String("filtersFile", "filters.csv", "Define a custom filters file")
+	Port = flag.String("PORT", "9090", "Port number to listen on")
 	J = flag.Int("j", 50, "Concurrent dbops count")
 	Testing = flag.Bool("testing", false, `controls testing mode
 	if the app is running on dry run mode following ops will not take place
