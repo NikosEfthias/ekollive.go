03764a70017c7681a18a3648a0ede3b6698549b3
nikos <nikos.efthias@gmail.com>
2017-09-26 06:26:35 +0400

    export flags as variables

--- lib/flags.go
+++ lib/flags.go
@@ -2,10 +2,22 @@ package lib
 
 import "flag"
 
-func ApplyFlags() {
-	flag.String("filtersFile", "filters.csv", "Define a custom filters file")
-	flag.Int("j", 50, "Concurrent dbops count")
-	flag.Bool("testing",false,`controls testing mode
+var (
+	FiltersFile *string
+	J           *int
+	Testing     *bool
+)
+
+func init() {
+	if !flag.IsParsed() {
+		flag.Parse()
+	}
+
+}
+func applyFlags() {
+	FiltersFile = flag.String("filtersFile", "filters.csv", "Define a custom filters file")
+	J = flag.Int("j", 50, "Concurrent dbops count")
+	Testing = flag.Bool("testing", false, `controls testing mode
 	if the app is running on dry run mode following ops will not take place
 		- nothing will be inserted into the db
 		- filters will be applied to everything regardless of matchid`)
