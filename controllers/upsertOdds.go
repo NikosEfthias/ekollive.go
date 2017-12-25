9af7f927b6985ad2f970cc905334cf07d8d06ef6
nikos <nikos.efthias@gmail.com>
2017-12-25 09:22:13 +0400

    no longer update each odd since its already done

--- controllers/upsertOdds.go
+++ controllers/upsertOdds.go
@@ -53,11 +53,11 @@ func UpsertOdds(match models.Match) {
 					Type:      of.Type,
 					Typeid:    of.Typeid,
 				}
-				_, err := db.DB.DB().Exec("UPDATE `odds` SET active=0 where matchid=? and oddFieldTypeId=? and oddTypeId=?",
-					*match.Matchid, *odf.Typeid, *od.Oddtypeid)
-				if nil != err {
-					fmt.Println("\x1B[0m", "error updating old odds", err, "\x1B[0m")
-				}
+				////////_, err := db.DB.DB().Exec("UPDATE `odds` SET active=0 where matchid=? and oddFieldTypeId=? and oddTypeId=?",
+				////////	*match.Matchid, *odf.Typeid, *od.Oddtypeid)
+				////////if nil != err {
+				////////	fmt.Println("\x1B[0m", "error updating old odds", err, "\x1B[0m")
+				////////}
 				db.Upsert(db.DB.DB(), "oddfieldtypes", odf)
 				data := &odd.Odd{
 					Oddid:          o.Id,
